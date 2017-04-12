d2893229559d7f349fff0d6c7d1bc1c44774e67d
Pradeep Ganesan <gp.esgan@gmail.com>
2017-04-11 19:47:17 -0700

    [flajoletmartin] code cleaned, removes testfile, bitsketchsize is passed as arg
    fix #2

--- flajoletmartin/estimator-num.py
+++ flajoletmartin/estimator-num.py
@@ -7,12 +7,10 @@ class FlajoletMartin(object):
 
     PHI = 1.2928
 
-    def __init__(self, filename):
+    def __init__(self, filename, sketchsize=32,):
         self.r = 0
-        self.M = 0
-        self.log_M = 0
         self.bitsketch = []
-        self.bitsketchdomain = []
+        self.bitsketchsize = sketchsize
         self.dataitems = []
         self.distinctitems = {}
 
@@ -25,37 +23,31 @@ class FlajoletMartin(object):
             else:
                 self.distinctitems[item] = 1
 
-        #self.M = len(self.dataitems)
-        self.M = 65536
-        #self.log_M = int(math.ceil(math.log(self.M, 2)))
-        self.log_M = 32
-        print('M: {} log M: {}'.format(self.M, self.log_M))
-
     def get_hash(self, item, seed):
-        """use a randomly chosen new hash function to get hash"""
+        """use a randomly chosen hash function to generate hash"""
         return mmh3.hash(item, seed)
 
     @staticmethod
     def ls1b(h):
         """position of least significant 1-bit"""
-        # hash is a 64bit number
+        # hash is a 32bit number
         return 32 if not h else (h&-h).bit_length()-1
 
     @staticmethod
     def ls0b(h):
-        """position of least significant 0-bit"""
+        """NOT USED - position of least significant 0-bit"""
         h_inv = ~h
-        # hash is a 64bit number
+        # hash is a 32bit number
         return 32 if not h else (h_inv&-h_inv).bit_length()-1
 
     @staticmethod
     def val_ls1b(h):
-        """2^(position of least significant 1-bit)"""
+        """NOT USED - 2^(position of least significant 1-bit)"""
         return h & (-h)
 
     @staticmethod
     def val_ls0b(h):
-        """2^(position of least significant 0-bit)
+        """NOT USED - 2^(position of least significant 0-bit)
         NOT USED"""
         return (~h) & (h + 1)
 
@@ -69,26 +61,13 @@ class FlajoletMartin(object):
         assert sum(self.distinctitems.values()) == len(self.dataitems)
         return len(self.distinctitems)
 
-    def build_bitsketch_domain(self):
-        """set bitsketch domain to
-        2^(position of least significant 1bit)"""
-        try:
-            for m in range(0, self.r):
-                for i in range(0, self.total_size()):
-                    h = self.get_hash(self.dataitems[i], m)
-                    self.bitsketchdomain[i*self.r + m] = self.val_ls1b(h)
-        except IndexError:
-            print('index error at : {}'.format(i*self.r + m))
-            raise
-
     def reset_bitsketch(self):
         assert self.r
-        self.bitsketch = [0] * self.log_M * self.r
+        self.bitsketch = [0] * self.bitsketchsize * self.r
 
-    def estimate_cardinality(self, r):
+    def estimate_cardinality(self, hash_group_size=32):
         """estimate set cardinality"""
-        assert r
-        self.r = r
+        self.r = hash_group_size
         R = 0.0
         self.reset_bitsketch()
 
@@ -97,25 +76,23 @@ class FlajoletMartin(object):
             for r in range(0, self.r):
                 for m in range(0, self.total_size()):
                     h = self.get_hash(self.dataitems[m], r)
-                    self.bitsketch[r*self.log_M+self.ls1b(h)] = 1
+                    self.bitsketch[r*self.bitsketchsize+self.ls1b(h)] = 1
         except IndexError:
             print('index error at : [{}][{}] for hash {}'.format(r, self.ls1b(h), h))
             raise
 
-        total, leftmostZero = 0.0, self.log_M
+        total, leftmostZero = 0.0, self.bitsketchsize
         for r in range(0, self.r):
-            for m in range(self.log_M-1, -1, -1):
-                if not self.bitsketch[r*self.log_M+m]:
+            for m in range(0, self.bitsketchsize, 1):
+                if not self.bitsketch[r*self.bitsketchsize+m]:
                     leftmostZero = m
-                    #break
+                    break
             total += leftmostZero
 
-        print('{}'.format(total/float(self.r)))
-        R = FlajoletMartin.PHI * pow(2.0, total/float(self.r))
-        return R
+        return FlajoletMartin.PHI * pow(2.0, total/float(self.r))
 
 def main():
-    estimator = FlajoletMartin('./testfile.txt')
+    estimator = FlajoletMartin('./testfile.txt', 32)
 
     print(
         'Total items: {} \nDistinct items count: {}\n'.format(
@@ -127,11 +104,13 @@ def main():
     print('Flajolet-Martin counts')
     r = 2
     while r <= 1024:
-        print('Estimated count for m[{}]: {}'.format(r, estimator.estimate_cardinality(r)))
-        #estimator.cleanup()
+        print(
+            'Estimated count for m[{}]: {}'.format(
+                r,
+                estimator.estimate_cardinality(r)
+                )
+        )
         r *= 2
 
 if __name__ == '__main__':
     main()
-
-
